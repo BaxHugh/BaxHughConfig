85d8a31c42b8b39902f52f2048ebea25fd8179d6
Hugh Baxter <hughdbaxter@gmail.com>
2023-09-19 14:45:19 +0100

    Add more info

--- README.md
+++ README.md
@@ -2,14 +2,22 @@
 A place to keep my config files for my Linux / Shell environment for easy install.
 And package installation script to quickly install the packages I need (using apt).
 
+# Shell Configuration
+There are custom shell agnostic `rc` and `env` files in the user's home directory which are sourced by the shell's config file.
+Currently zsh and bash are supported.
+Having separate custom `rc` and `env` files allows a user to more easily manage their shell environment separate from any defaults or third party tools.
+I.e. adding specific directories to their path.
+
 # Shell Config Installation
+
 ```bash
 curl -fsSL https://raw.githubusercontent.com/BaxHugh/BaxHughConfig/main/shell-config-install.sh | bash -s --install-zsh
 ```
 See `shell-config-install.sh --help` for more options.
 ```
 Usage: shell-config-install.sh [options]
-Configure the shell environment to the default for BaxHugh
+Configure the shell environment to the default used by BaxHugh
+This includes shell config + env files, custom tool config files, and setting the oh-my-zsh theme
 Options:
   --no-backup,              Don't backup existing files
   --install-zsh             Install zsh (and oh-my-zsh) if not already installed
@@ -17,6 +25,36 @@ Options:
   --unattended,             Answer no to any prompts
   -h, --help,               Display this help message
 ```
+
+<!-- This is copies from the help output -->
+
+**What this script does**
+This command will install the below to the user's home directory.
+If a file already exists, it will be backed up to <filename>~ unless --no-backup is passed.
+
+**Custom shell config + env files**
+These will be installed in the user's home directory, and sourced in their appropriate shell config files.
+| File             | Description                                     | Sourced in               |
+| ---------------- | ----------------------------------------------- | ------------------------ |
+| `.zshrc_config`  | Custom zsh config                               | `.zshrc`                 |
+| `.bashrc_config` | Custom bash config                              | `.bashrc`                |
+| `.shenv_custom`  | Custom shell environment setup (shell agnostic) | `.zshenv` and `.profile` |
+| `.shrc_custom`   | Custom shell config (shell agnostic)            | `.zshrc` and `.bashrc`   |
+
+**Custom shell config files (machine specific)**
+These files will be sourced if the user creates them
+| File                  | Description                                     | Sourced in                          |
+| --------------------- | ----------------------------------------------- | ----------------------------------- |
+| `.shrc_custom_local`  | Custom shell config (shell agnostic)            | sourced in `.zshrc` and `.bashrc`   |
+| `.shenv_custom_local` | Custom shell environment setup (shell agnostic) | sourced in `.zshenv` and `.profile` |
+
+**Custom tool config files**
+These will be installed in the user's home directory.
+| File          | Description            |
+| ------------- | ---------------------- |
+| `.inputrc`    | Custom readline config |
+| `.nanorc`     | Custom nano config     |
+| `.style.yapf` | Custom yapf config     |
 
 # Package Installation
 ```bash
--- shell-config-install.sh
+++ shell-config-install.sh
@@ -10,13 +10,48 @@ _omzsh_is_installed() {
 
 _help() {
     echo "Usage: shell-config-install.sh [options]"
-    echo "Configure the shell environment to the default for BaxHugh"
+    echo "Configure the shell environment to the default used by BaxHugh"
+    echo "This includes shell config + env files, custom tool config files, and setting the oh-my-zsh theme"
     echo "Options:"
     echo "  --no-backup,              Don't backup existing files"
     echo "  --install-zsh             Install zsh (and oh-my-zsh) if not already installed"
-    echo "  --install-baxhugh-bin,    Install BaxHughBin to ~/.bin"
+    echo "  --install-baxhugh-bin,    Install BaxHughBin to ~/.bin (see https://github.com/BaxHugh/BaxHughBin)"
     echo "  --unattended,             Answer no to any prompts"
     echo "  -h, --help,               Display this help message"
+    echo ""
+    echo "More info:"
+    echo ""
+    echo -e "\e[1mWhat this command does\e[0m"
+    echo "This command will install the below to the user's home directory."
+    echo "If a file already exists, it will be backed up to <filename>~ unless --no-backup is passed."
+    echo ""
+    echo -e "\e[1mCustom shell config + env files\e[0m"
+    echo "These will be installed in the user's home directory, and sourced in their appropriate shell config files."
+    echo ""
+    echo '| File                  | Description                                     | Sourced in                          |'
+    echo '| --------------------- | ----------------------------------------------- | ----------------------------------- |'
+    echo '| `.zshrc_config`       | Custom zsh config                               | `.zshrc`                            |'
+    echo '| `.bashrc_config`      | Custom bash config                              | `.bashrc`                           |'
+    echo '| `.shenv_custom`       | Custom shell environment setup (shell agnostic) | `.zshenv` and `.profile`            |'
+    echo '| `.shrc_custom`        | Custom shell config (shell agnostic)            | `.zshrc` and `.bashrc`              |'
+    echo ''
+    echo -e "\e[1mCustom shell config files (machine specific)\e[0m"
+    echo  "These files will be sourced if the user creates them"
+    echo ""
+    echo '| File                  | Description                                     | Sourced in                          |'
+    echo '| --------------------- | ----------------------------------------------- | ----------------------------------- |'
+    echo '| `.shrc_custom_local`  | Custom shell config (shell agnostic)            | sourced in `.zshrc` and `.bashrc`   |'
+    echo '| `.shenv_custom_local` | Custom shell environment setup (shell agnostic) | sourced in `.zshenv` and `.profile` |'
+    echo ''
+    echo -e "\e[1mCustom tool config files\e[0m"
+    echo "These will be installed in the user's home directory."
+    echo ""
+    echo '| File                  | Description                                     |'
+    echo '| --------------------- | ----------------------------------------------- |'
+    echo '| `.inputrc`            | Custom readline config                          |'
+    echo '| `.nanorc`             | Custom nano config                              |'
+    echo '| `.style.yapf`         | Custom yapf config                              |'
+    echo ""
 }
 
 while [[ $# -gt 0 ]]; do
